28701fc6fef20baa07979fb5133e19d21c6ceb65
Sarvesh Avhad <sarveshavhad4@gmail.com>
2026-07-19 20:52:01 +0530

    Time: 3 ms (26.15%) | Memory: 44.5 MB (81.93%) - LeetSync

---
+++ 1468-check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean checkIfExist(int[] arr) {
+        for(int i=0;i<arr.length-1;i++){
+            for(int j=i+1;j<arr.length;j++){
+                if(arr[i] == arr[j]*2 || arr[i] == arr[j]/2.0) return true;
+            }
+        }
+    return false;
+    }
+}
