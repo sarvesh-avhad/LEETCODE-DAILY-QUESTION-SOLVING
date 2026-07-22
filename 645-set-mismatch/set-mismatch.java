b12f479e22f22321904afb63c4960212cf19dcb5
Sarvesh Avhad <sarveshavhad4@gmail.com>
2026-07-22 19:01:01 +0530

    Time: 19 ms (13.67%) | Memory: 47.7 MB (49.72%) - LeetSync

---
+++ 645-set-mismatch/set-mismatch.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] findErrorNums(int[] nums) {
+
+        int[] ans = new int[2];
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+
+        for (int i = 1; i <= nums.length; i++) {
+            if (!map.containsKey(i)) {
+                ans[1] = i;       
+            } else if (map.get(i) == 2) {
+                ans[0] = i;        
+            }
+        }
+
+        return ans;
+    }
+}
